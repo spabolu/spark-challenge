b35896a05f98f0f1105d9db6fbd725ca37eaa109
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2024-09-08 20:12:00 -0700

    change styling

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -22,7 +22,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="fantasy">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
