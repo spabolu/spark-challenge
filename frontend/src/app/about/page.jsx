838d2d2da4cf1035ea61de548d57efb9181ade3b
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2024-09-08 19:46:27 -0700

    nextjs init

---
+++ frontend/src/app/about/page.jsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>About Page</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <nav className="mx-auto flex h-16 w-full items-center justify-between bg-zinc-800 px-2 font-bold md:px-20">
+      <Link
+        href="/"
+        className="flex items-center transition-all focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 md:space-x-0.5"
+      >
+        <h1 className="text-lg text-white">mineTALK</h1>
+      </Link>
+
+      <div className="flex items-center space-x-2 md:space-x-4 ">
+        <Link
+          href="/data"
+          className="font-medium text-white transition-all focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Data
+        </Link>
+
+        <Link
+          href="/about"
+          className="font-medium text-white transition-all focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          About
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
