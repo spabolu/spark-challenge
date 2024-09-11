660820bad42e01af8ff91746db4f115427093a4d
Khushi <ec2-user@ip-172-31-18-88.ec2.internal>
2024-09-11 00:03:50 +0000

    database for frontend

--- frontend/pages/api/posts.js
+++ frontend/pages/api/posts.js
@@ -3,10 +3,13 @@
 import excuteQuery from '@/lib/db';
 
 export default async function handler(req, res) {
+  console.log("in the posts api");
   if (req.method === 'GET') {
+    console.log("request was GET");
     try {
-      const results = await excuteQuery('SELECT * FROM feedback');
+      const results = await excuteQuery({ query: 'SELECT * FROM feedback' });
       res.status(200).json(results);
+      console.log(results)
     } catch (error) {
       console.error('Database error:', error);
       res.status(500).json({ error: 'Error retrieving feedback' });
@@ -15,16 +18,3 @@ export default async function handler(req, res) {
     res.status(405).json({ error: 'Method not allowed' });
   }
 }
-
-// export default async (req, res) => {
-//   try {
-//     console.log('req nom', req.body);
-//     const result = await excuteQuery({
-//       query: 'INSERT INTO post(content) VALUES(?)',
-//       values: [req.body.content],
-//     });
-//     console.log('ttt', result);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
