f014b61eb4ba0f00eb6afd459ae29e614a83b007
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2024-09-11 02:11:10 -0700

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <nav className="mx-auto flex h-16 w-full items-center justify-between bg-zinc-800 px-2 font-bold md:px-20">
+    <nav className="mx-auto flex h-16 w-full items-center justify-between px-2 font-bold md:px-20 bg-zinc-800">
       <Link
         href="/"
         className="flex items-center transition-all focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 md:space-x-0.5"
       >
-        <h1 className="text-lg text-white">mineTALK</h1>
+        <h1 className="text-xl text-white">mineTALK</h1>
       </Link>
 
       <div className="flex items-center space-x-2 md:space-x-4 ">
